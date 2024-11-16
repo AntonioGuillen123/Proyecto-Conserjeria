35c0a45aa002d9b6e877d9b6018d8ef2a0708a87
talenaa <talenabarbosa7@gmail.com>
2024-11-17 00:07:58 +0100

    style: added github link

--- src/Views/Components/footer.php
+++ src/Views/Components/footer.php
@@ -1,6 +1,10 @@
 <footer>
     <div class="container">
-        <p>© The Concierge Hotel.</p>
+        <p>© Hotel Concierge.</p>
+            <a href="https://github.com/Matias-Stadler"><img class="gitIcons" src="./public/img/gitHubIcon.png" alt="gitIcon"></a>
+            <a href="https://github.com/AntonioGuillen123"><img class="gitIcons" src="./public/img/gitHubIcon.png" alt="gitIcon"></a>
+            <a href="https://github.com/Jaz356"><img class="gitIcons" src="./public/img/gitHubIcon.png" alt="gitIcon"></a>
+            <a href="https://github.com/talenaa"><img class="gitIcons" src="./public/img/gitHubIcon.png" alt="gitIcon"></a>
         <p> All rights reserved.</p>
     </div>
 </footer>
--- src/Views/Components/header.php
+++ src/Views/Components/header.php
@@ -3,8 +3,8 @@
     <div class="headerContent">
         <img class="logoHotel" src="./public/img/logoHeader.jpg" alt="logo">
         <div class="textHeader">
-            <p>Welcome to the Concierge Hotel management system.</p>
-            <p> We expect the unexpected.</p>
+            <p>Welcome to the Hotel Concierge management system.</p>
+            <p>We expect the unexpected.</p>
         </div>
     </div>
 </header>
