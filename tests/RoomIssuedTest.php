8ccd7bdcbdcd4e0fa5ca851a8b3536f82c1fe789
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2024-11-17 14:58:31 +0100

    feat: Add a test to check get room

--- tests/RoomIssuedTest.php
+++ tests/RoomIssuedTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests;
 
+use App\Models\RoomIssued;
 use PHPUnit\Framework\TestCase;
 
 class RoomIssuedTest extends TestCase
@@ -10,5 +11,16 @@ class RoomIssuedTest extends TestCase
     public function testIfTestOk()
     {
         $this->assertEquals(1, 1);
+    }
+
+    public function test_ifGetRoomWorkCorrectly(){
+        // given - Actores de la película
+        $roomIssued = new RoomIssued(null, 'Room test 1', 'Issue test 1', 'Area test 1', null);
+
+        // when - Rodaje de la película
+        $resultGetRoom = $roomIssued->getRoom();
+
+        // then - Crítica de la película
+        $this->assertEquals('Room test 1', $resultGetRoom);
     }
 }
