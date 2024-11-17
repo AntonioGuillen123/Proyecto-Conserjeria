ed00f11c008e271a6b18139260a551c7e01d38f9
Antonio Guillén García <119064532+AntonioGuillen123@users.noreply.github.com>
2024-11-17 14:52:59 +0100

    feat: Creation of verifier test

---
+++ tests/RoomIssuedTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class RoomIssuedTest extends TestCase
+{
+
+    public function testIfTestOk()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
