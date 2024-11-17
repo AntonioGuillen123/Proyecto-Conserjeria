5034a783311f582a4208f6303373c27d30dfaceb
talenaa <talenabarbosa7@gmail.com>
2024-11-17 15:47:42 +0100

    style: centered table

--- src/Views/Components/layout.php
+++ src/Views/Components/layout.php
@@ -5,6 +5,5 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link href="./src/css/style.css" rel="stylesheet">
-        <script src="https://cdn.tailwindcss.com"></script>
     </head>
 </html>
